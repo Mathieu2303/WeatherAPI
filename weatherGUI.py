62f2abab1f48ddf7de52809068bffa1ff290ba4e
Mathieu2303 <mathieupartain@gmail.com>
2024-04-10 16:24:06 -0700

    temp from kelvin to fahrenheit

--- weatherGUI.py
+++ weatherGUI.py
@@ -5,14 +5,14 @@ from main import get_weather_data
 def get_city():
     question = city_entry.get()
     response = get_weather_data(question)
-    print(response)
+    print(response, " degrees fahrenehiet")
     return response
 
     
     
 master = tk.Tk()
 
-master.config(width =400,height = 300)
+master.config(width = 800,height = 800)
 master.title("Weather Application")
 
 question_label = tk.Label(master, text = "what city would you like to know the tempature for? ")
@@ -23,6 +23,5 @@ city_entry.pack()
 
 print_button = tk.Button(master, text = "submit", command = get_city)
 print_button.pack()
-
 
 master.mainloop()
